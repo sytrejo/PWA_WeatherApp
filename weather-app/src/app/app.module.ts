151bce37817cee322fc793620a53b7685f185d47
sytrejo <s.y.trejo26@gmail.com>
2023-08-07 17:17:20 -0400

    adding the weather service class and http to weather service

--- weather-app/src/app/app.module.ts
+++ weather-app/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import{ HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -10,7 +11,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
