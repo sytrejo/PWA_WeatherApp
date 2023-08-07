a0bbc39b4954183d871ce80c07d3e931aaa974e7
sytrejo <s.y.trejo26@gmail.com>
2023-08-07 18:48:01 -0400

    updates

--- weather-app/src/environments/environment.prod.ts
+++ weather-app/src/environments/environment.prod.ts
@@ -1,2 +1,5 @@
-apiUrl: 'https://api.openweathermap.org/data/2.5.',
-apiKey: '9b0ebd4102b55b02b4d7400e2d5f854e'
+export const environment ={
+    production: true,
+    apiUrl: 'https://api.openweathermap.org/data/2.5/',
+    apiKey: ''
+}
