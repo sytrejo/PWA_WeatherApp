e8bbadd8df92ea57f9fef39ead2bdaccc8db660b
sytrejo <s.y.trejo26@gmail.com>
2023-08-07 19:02:01 -0400

    updates

--- weather-app/src/app/app.module.ts
+++ weather-app/src/app/app.module.ts
@@ -7,10 +7,13 @@ import { MatInputModule } from '@angular/material/input';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { WeatherComponent } from './weather/weather.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WeatherComponent,
+  
   ],
   imports: [
     BrowserModule,
